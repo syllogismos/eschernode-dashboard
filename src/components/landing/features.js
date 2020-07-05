bc3d93a7796f972f5bf818ca90c169e790937800
Anil Karaka <anilkaraka@live.in>
2020-07-05 18:58:00 +0530

    update landing page

--- src/components/landing/features.js
+++ src/components/landing/features.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Row, Card, CardBody, Jumbotron, CardImg } from 'reactstrap';
-import { Colxx } from '../common/CustomBootstrap';
+import { Colxx, Separator } from '../common/CustomBootstrap';
 
 const Features = () => {
   return (
@@ -10,9 +10,53 @@ const Features = () => {
           <Card>
             <CardBody>
               <Jumbotron>
-                <h1 className="display-4">Features</h1>
+                <h1 className="display-4">Filter your followers</h1>
+                <p className="lead">
+                  Create custom segments of your followers.
+                </p>
+
                 <img
-                  src="/assets/eschernode/filters.png"
+                  src="/assets/eschernode/filters1.png"
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              </Jumbotron>
+            </CardBody>
+          </Card>
+        </Colxx>
+      </Row>
+      <Row>
+        <Colxx xxs="12" className="mb-4">
+          <Card>
+            <CardBody>
+              <Jumbotron>
+                <h1 className="display-4">Messaging Campaign</h1>
+                <p className="lead">
+                  Compose your message, send it to your target segment.
+                </p>
+                <img
+                  src="/assets/eschernode/campaign1.png"
+                  alt=""
+                  width="100%"
+                  height="100%"
+                />
+              </Jumbotron>
+            </CardBody>
+          </Card>
+        </Colxx>
+      </Row>
+      <Row>
+        <Colxx xxs="12" className="mb-4">
+          <Card>
+            <CardBody>
+              <Jumbotron>
+                <h1 className="display-4">Campaign Stats</h1>
+                <p className="lead">
+                  Track, clicks and conversions of your campaign.
+                </p>
+                <img
+                  src="/assets/eschernode/stats.png"
                   alt=""
                   width="100%"
                   height="100%"
